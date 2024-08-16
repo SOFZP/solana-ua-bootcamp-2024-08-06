60d11a6a95254080b359f112780ebc903a59b1c7
Vik <vb.zp@ukr.net>
2024-08-16 22:21:18 +0300

    Add initial TypeScript files and environment configuration to practice-1

---
+++ practice-1/check-balance.ts
@@ -0,0 +1,30 @@
+import "dotenv/config";
+import {
+  Connection,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  clusterApiUrl,
+} from "@solana/web3.js";
+import {
+  airdropIfRequired,
+} from "@solana-developers/helpers";
+
+const connection = new Connection(clusterApiUrl("devnet"));
+console.log(`⚡️ Connected to devnet`);
+
+const publicKey = new PublicKey("6FVoYDbUXD1EaHcJfCBrdA6A9ch8JYzz4BJb7EzcFssf");
+
+/* await airdropIfRequired(
+  connection,
+  publicKey,
+  1 * LAMPORTS_PER_SOL,
+  0.5 * LAMPORTS_PER_SOL
+); */
+
+const balanceInLamports = await connection.getBalance(publicKey);
+
+const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
+
+console.log(
+  `💰 The balance for the wallet at address ${publicKey} is: ${balanceInSOL}`
+);
